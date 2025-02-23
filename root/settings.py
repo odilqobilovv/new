26ddce27921f94fc01374d2670f910e41dec9405
QOBILOV-ODILJON <qobilovodiljonoff@gmail.com>
2025-02-23 16:06:35 +0500

    commitee

--- root/settings.py
+++ root/settings.py
@@ -19,7 +19,7 @@ DEBUG=(bool, False)
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-d6xln)*==7jv09$^s3q+)fvl3pygwrx)mfg-mkr-2ezuc(wqb0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
