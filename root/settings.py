a5422cdbfc50a6c361065eaba23af8c4d443abc3
QOBILOV-ODILJON <qobilovodiljonoff@gmail.com>
2025-02-23 16:24:19 +0500

    commitee

--- root/settings.py
+++ root/settings.py
@@ -125,12 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
-CORS_ALLOW_HEADERS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+
 
 
 STATIC_URL = 'static/'
